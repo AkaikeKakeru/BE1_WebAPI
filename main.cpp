39ee882ca15abc85a5cef11092815464bc6a629d
agane <k021g1137@g.neec.ac.jp>
2024-09-27 01:29:57 +0900

    POSTを改良。urlを引数で受け取り、戻り値をJsonに変更。

--- main.cpp
+++ main.cpp
@@ -11,44 +11,44 @@ using namespace concurrency::streams;   // 非同期ストリーム
 
 template <class T>
 
-pplx::task<T> GetAllFaculties(const std::wstring &url) {
+pplx::task<T> GetAllFaculties(const std::wstring& url) {
 	return pplx::create_task([=] {
 		// クライアントの設定
 		http_client client(url);
 
 		// リクエスト送信
 		return client.request(methods::GET);
-	})
-	.then([](http_response response) {
-		// ステータスコート判定
-		if (response.status_code() == status_codes::OK) {
-			return response.extract_json();
-		}
-	});
+		})
+		.then([](http_response response) {
+			// ステータスコート判定
+			if (response.status_code() == status_codes::OK) {
+				return response.extract_json();
+			}
+			});
 }
 
-
-pplx::task<void> PostTest() {
-	return pplx::create_task([] {
-		// クライアントの設定
-		http_client client(L"http://localhost:3000/faculties");
-
+pplx::task<int> Post(const std::wstring& url) {
+	return pplx::create_task([=] {
 		// 送信データの作成
 		json::value postData;
-		postData[L"name"] = json::value::string(L"test");
+		postData[L"name"] = json::value::string(L"ハイパーL4科");
 
+		// クライアントの設定
+		http_client client(url);
 		// リクエスト送信
 		return client.request(methods::POST, L"", postData.serialize(),
 			L"application/json");
-	})
-	.then([](http_response response) {
-		// ステータスコート判定
-		if (response.status_code() == status_codes::OK) {
-			// レスポンスボディを表示
-			auto body = response.extract_string();
-			std::wcout << body.get().c_str() << std::endl;
-		}
-	});
+		})
+		.then([](http_response response) {
+			// ステータスコート判定
+			if (response.status_code() == status_codes::Created) {
+				// レスポンスボディを表示
+				return response.extract_json();
+			}
+			})
+			.then([](json::value json) {
+				return json[L"rowCount"].as_integer();
+				});
 }
 
 int main() {
@@ -59,16 +59,24 @@ int main() {
 			GetAllFaculties<json::value>(L"http://localhost:3000/faculties/");
 		const json::value j = task.get();
 		web::json::array array = j.as_array();
-		for(int i = 0; i < j.size(); i++) {
+		for (int i = 0; i < j.size(); i++) {
 			std::wcout << array[i].at(U("name")) << std::endl;
 		}
-	} catch (const std::exception &e) {
+	}
+	catch (const std::exception& e) {
 		printf("Error exception:%s\n", e.what());
 	}
 
 	try {
-		PostTest().wait();
-	} catch (const std::exception &e) {
+		//Post
+		auto response =
+			Post(L"http://localhost:3000/faculties/").get();
+
+		if (response == 1) {
+			std::cout << "新規データを追加しました。" << std::endl;
+		}
+	}
+	catch (const std::exception& e) {
 		printf("Error exception:%s\n", e.what());
 	}
 
