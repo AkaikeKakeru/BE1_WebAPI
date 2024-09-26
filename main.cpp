43afb79076150d430965ea34b37ee1aee09ab6c4
agane <k021g1137@g.neec.ac.jp>
2024-09-27 01:04:49 +0900

    GETを改良。urlを引数で受け取り、戻り値をJsonに変更。

--- main.cpp
+++ main.cpp
@@ -9,10 +9,12 @@ using namespace web::http;              // 共通のHTTP機能
 using namespace web::http::client;      // HTTP クライアントの機能
 using namespace concurrency::streams;   // 非同期ストリーム
 
-pplx::task<void> GetAllFaculties() {
-	return pplx::create_task([] {
+template <class T>
+
+pplx::task<T> GetAllFaculties(const std::wstring &url) {
+	return pplx::create_task([=] {
 		// クライアントの設定
-		http_client client(L"http://localhost:3000/faculties");
+		http_client client(url);
 
 		// リクエスト送信
 		return client.request(methods::GET);
@@ -20,10 +22,7 @@ pplx::task<void> GetAllFaculties() {
 	.then([](http_response response) {
 		// ステータスコート判定
 		if (response.status_code() == status_codes::OK) {
-			// レスポンスボディを表示
-			auto body = response.extract_string();
-			setlocale(LC_CTYPE, "ja_JP.UTF-8");
-			std::wcout << body.get().c_str() << std::endl;
+			return response.extract_json();
 		}
 	});
 }
@@ -53,8 +52,16 @@ pplx::task<void> PostTest() {
 }
 
 int main() {
+	setlocale(LC_CTYPE, "ja_JP.UTF-8");
 	try {
-		GetAllFaculties().wait();
+		//Get
+		concurrency::task<web::json::value> task =
+			GetAllFaculties<json::value>(L"http://localhost:3000/faculties/");
+		const json::value j = task.get();
+		web::json::array array = j.as_array();
+		for(int i = 0; i < j.size(); i++) {
+			std::wcout << array[i].at(U("name")) << std::endl;
+		}
 	} catch (const std::exception &e) {
 		printf("Error exception:%s\n", e.what());
 	}
