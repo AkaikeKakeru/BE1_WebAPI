a86cb9bb0b8a59625ccbb5d81044c39be1865a48
agane <k021g1137@g.neec.ac.jp>
2024-09-26 22:00:34 +0900

    プロジェクト ファイルを追加します。

---
+++ BE1_03.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+#include <cpprest/filestream.h>
+#include <cpprest/http_client.h>
+
+using namespace utility;
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace concurrency::streams;
+
+int main() {
+    std::cout << "Hello World!\n";
+}
